303b45901fbd7c23dec55378bf3a35e0d933e375
BartekKow <115708938+BartekKow@users.noreply.github.com>
2025-02-17 20:14:33 +0100

    add

--- dev-spring-boot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/dao/StudentDAOlmpl.java
+++ dev-spring-boot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/dao/StudentDAOlmpl.java
@@ -1,13 +1,30 @@
 package com.luv2code.cruddemo.dao;
 
 import com.luv2code.cruddemo.entity.Student;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public class StudentDAOlmpl implements StudentDAO {
 
+    // define field for entity manager
+
+    private EntityManager entityManager;
+
+    // inject entity manger using constructor injection
+
+    @Autowired
+    public StudentDAOlmpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    // implement save method
+
     @Override
+    @Transactional
     public void save(Student theStudent) {
-        System.out.println("save Student");
+        entityManager.persist(theStudent);
     }
 }
