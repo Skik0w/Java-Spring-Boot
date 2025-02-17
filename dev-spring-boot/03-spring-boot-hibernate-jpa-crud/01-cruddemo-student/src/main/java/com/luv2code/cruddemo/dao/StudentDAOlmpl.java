b6b600db692163e6c7cb28388183fccceb9ebd5f
BartekKow <115708938+BartekKow@users.noreply.github.com>
2025-02-17 19:15:08 +0100

    add

---
+++ dev-spring-boot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/dao/StudentDAOlmpl.java
@@ -0,0 +1,13 @@
+package com.luv2code.cruddemo.dao;
+
+import com.luv2code.cruddemo.entity.Student;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentDAOlmpl implements StudentDAO {
+
+    @Override
+    public void save(Student theStudent) {
+
+    }
+}
